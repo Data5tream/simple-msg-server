a668fb729008e6c007671178e5b2d94da723b69b
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-11-15 16:24:33 +0100

    add msg entry view, link stuff in tables and enable dark-mode

--- simple_msg_server/urls.py
+++ simple_msg_server/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.DashboardView.as_view(), name='index'),
-    path('form/<int:pk>', views.FormDetailView.as_view(), name='form_detail')
+    path('form/<int:pk>', views.FormDetailView.as_view(), name='form_detail'),
+    path('form/entry/<int:pk>', views.FormEntryDetailView.as_view(), name='form_entry'),
     # path('login', auth_views.LoginView.as_view(), name='login'),
     # path('logout', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- simple_msg_server/views.py
+++ simple_msg_server/views.py
@@ -1,20 +1,26 @@
-import json
-
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView, DetailView
 
 from simple_msg_server.models import MsgForm, MsgEntry
 
 
+@method_decorator(login_required, name='dispatch')
 class DashboardView(TemplateView):
     template_name = 'simple_msg_server/dashboard.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['forms'] = MsgForm.objects.all()
-        context['msgs'] = MsgEntry.objects.all()[:10]
+        context['entries'] = MsgEntry.objects.all().order_by('-created')[:10]
         return context
 
 
 class FormDetailView(DetailView):
     model = MsgForm
     context_object_name = 'form'
+
+
+class FormEntryDetailView(DetailView):
+    model = MsgEntry
+    context_object_name = 'entry'
