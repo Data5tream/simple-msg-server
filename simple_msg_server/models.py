1aef08311ddcf58f8f29a2e291d5a2835516930d
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-11-15 11:26:50 +0100

    setup base models

--- simple_msg_server/models.py
+++ simple_msg_server/models.py
@@ -1,4 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class MsgForm(models.Model):
+    name = models.CharField(max_length=128)
+    desc = models.CharField(max_length=512)
+    fields = models.JSONField()
+    endpoint = models.URLField()
+
+    def __str__(self):
+        return f'{self.id} - {self.name}'
+
+
+class MsgEntry(models.Model):
+    form = models.ForeignKey('MsgForm', on_delete=models.CASCADE)
+    content = models.JSONField()
+    created = models.DateTimeField(auto_now_add=True)
+    read_by = models.ForeignKey('User', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.id} - Answer to {self.form.name} ({self.form_id}) - {self.created}'
