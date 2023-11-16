0fa090d03f0a6d45f1403a5a4951f0b8059695e7
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-11-16 16:10:47 +0100

    improve navbar and add logout

--- simple_msg_server/urls.py
+++ simple_msg_server/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('form/entry/<int:pk>', views.FormEntryDetailView.as_view(), name='form_entry'),
     path('collect_data/<uuid:endpoint>', views.CollectFormEntryView.as_view(), name='collect_form_entry'),
     path('login', auth_views.LoginView.as_view(template_name='simple_msg_server/login.html'), name='login'),
-    # path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout', auth_views.LogoutView.as_view(template_name='simple_msg_server/logout.html'), name='logout'),
 ]
